96198acf8abb14e7e570b6923737414459344f69
anastasiagh <gheorghita.anastasia@isa.utm.md>
2022-02-12 18:40:58 +0200

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Main from './components/MainComponent';
 import './App.css';
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
 
@@ -14,20 +15,17 @@ class Main extends Component {
   }
 
   onDishSelect(dishId) {
-    this.setState({selectedDish: dishId});
+    this.setState({ selectedDish: dishId });
   }
 
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} 
-            onClick={(dishId) => this.onDishSelect(dishId)} />
+      <div>
+        <Header />
+        <Menu dishes={this.state.dishes}
+          onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Footer />
       </div>
     );
   }
